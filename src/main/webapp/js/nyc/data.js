1818dd43a3ece380e0c5448f713c699add979496
timkeane <tkeane@doitt.nyc.gov>
2016-04-20 15:28:01 -0400

    new data

--- src/main/webapp/js/nyc/data.js
+++ src/main/webapp/js/nyc/data.js
@@ -3,174 +3,174 @@ window.nyc = window.nyc || {};
 nyc.data = {
 	beaches: {
 		'sea gate beach club': {
-			lineString: [[983593, 147787], [982910, 147996]], 
+			lineString: [[983572, 147734], [982957, 148027]], 
 			labelPointer: [[983274, 147885], [972765, 169983]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'right'
-		},
+		}, 
 		'manhattan beach': {
-			lineString: [[999052, 148774], [999442, 148916], [1000010, 148987], [1000455, 148924]], 
-			labelPointer: [[999769, 148955], [984365, 125479]],
-			alignPointer: 'right',
+			lineString: [[999157, 148847], [999175, 148962], [999249, 149071], [999461, 149199], [999766, 149262], [1000058, 149265], [1000274, 149166], [1000365, 148953]], 
+			labelPointer: [[999769, 148955], [984365, 125479]], 
+			alignPointer: 'right', 
 			alignLine: 'right'
-		},
+		}, 
 		'locust point yacht club': {
-			lineString: [[1038622, 235398], [1039124, 235443]], 
-			labelPointer: [[1038692, 235535], [1053437, 238273]],
-			alignPointer: 'left',
+			lineString: [[1038585, 236100], [1038763, 235822], [1038852, 235605], [1038807, 235403]], 
+			labelPointer: [[1038692, 235535], [1053437, 238273]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'schuyler hill civic association': {
-			lineString: [[1038321, 235397], [1038405, 235287]], 
-			labelPointer: [[1038238, 235273], [1053437, 232773]],
-			alignPointer: 'left',
+			lineString: [[1038181, 235320], [1038206, 235246], [1038253, 235167]], 
+			labelPointer: [[1038238, 235273], [1053437, 232773]], 
+			alignPointer: 'left', 
 			alignLine: 'right'
-		},
+		}, 
 		'whitestone booster civic association': {
-			lineString: [[1034701, 230634], [1034550, 230766]], 
+			lineString: [[1034647, 230551], [1034412, 230777]], 
 			labelPointer: [[1034704, 230637], [1017736, 230637]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'wolfe\'s pond beach': {
-			lineString: [[931918, 127361], [931353, 126758]], 
-			labelPointer: [[932053, 126707], [920448, 126707]],
-			alignPointer: 'right',
+			lineString: [[933028, 128650], [932716, 128439], [932375, 128099], [932076, 127806], [931603, 127178], [931445, 127030], [931093, 126550], [930874, 126274], [930615, 125998]], 
+			labelPointer: [[932053, 126707], [920448, 126707]], 
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'midland beach': {
-			lineString: [[963117, 149912], [957924, 144290]], 
+			lineString: [[963041, 150003], [961778, 148779], [960558, 147603], [959233, 146257], [958010, 144383]], 
 			labelPointer: [[959554, 147287], [947958, 147302]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'south beach': {
-			lineString: [[967801, 155653], [963195, 150014]], 
-			labelPointer: [[965363, 154201], [947958, 152213]],
-			alignPointer: 'right',
+			lineString: [[967617, 155679], [967088, 155181], [966550, 154539], [963041, 150003]], 
+			labelPointer: [[965363, 154201], [947958, 152213]], 
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'gerritsen / kiddie beach': {
-			lineString: [[1005822, 152729], [1005353, 152699]], 
-			labelPointer: [[1005579, 152710], [1018102, 161483]],
-			alignPointer: 'left',
+			lineString: [[1005629, 152792], [1005564, 152805], [1005495, 152807], [1005431, 152793], [1005384, 152767]], 
+			labelPointer: [[1005579, 152710], [1018102, 161483]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'kingsborough community college': {
-			lineString: [[1001858, 149074], [1002040, 149146], [1002194, 149165], [1002383, 149032]], 
-			labelPointer: [[1002150, 149159], [1013745, 146159]],
-			alignPointer: 'left',
+			lineString: [[1001880, 149121], [1001980, 149153], [1002066, 149160], [1002151, 149139], [1002298, 149026]], 
+			labelPointer: [[1002150, 149159], [1013745, 146159]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'sea gate 42nd': {
 			lineString: [[981907, 148433], [982534, 148162]], 
 			labelPointer: [[982240, 148289], [972765, 163538]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'coney island beach': {
-			lineString: [[996871, 148555], [995334, 148563], [993434, 148327], [991017, 147832], [989546, 147597], [986360, 147207], [984359, 147207], [983623, 147569]], 
+			lineString: [[996902, 148687], [995152, 148604], [993318, 148451], [990901, 148035], [989430, 147800], [986214, 147271], [985152, 147298], [984117, 147375], [983572, 147734]], 
 			labelPointer: [[990385, 147732], [977765, 131707]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'rockaway beach': {
-			lineString: [[1022308, 145961], [1027983, 148923], [1035164, 151738], [1037708, 152566], [1039096, 152512], [1042940, 153593], [1046474, 154612], [1048112, 154628], [1050559, 154999], [1052350, 154837], [1053245, 155308], [1053797, 155625], [1054953, 156197], [1055453, 155962], [1055397, 155962]], 
+			lineString: [[1022321, 145917], [1027995, 148793], [1032557, 150667], [1035177, 151694], [1037902, 152588], [1039048, 152685], [1042277, 153476], [1044812, 154280], [1046487, 154568], [1048632, 154752], [1050569, 154906], [1052817, 154916], [1053225, 155342], [1053774, 155689], [1054962, 156185], [1055296, 156279], [1055434, 156068]], 
 			labelPointer: [[1036329, 151136], [1047923, 151136]], 
-			alignPointer: 'left',
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'breezy point reid ave': {
-			lineString: [[1003314, 142433], [1003673, 142637], [1003939, 142862], [1004392, 143423], [1004905, 143798], [1005349, 143852], [1006078, 143913], [1006919, 144128], [1007704, 144612], [1008338, 145075], [1008686, 145106]], 
-			labelPointer: [[1005966, 143902], [1032215, 138327]],
-			alignPointer: 'left',
+			lineString: [[1003254, 142502], [1003664, 142729], [1003965, 142982], [1004381, 143362], [1004691, 143696], [1004993, 143849], [1005389, 143958], [1005980, 143905], [1006701, 144041], [1007191, 144223], [1008311, 144934], [1008686, 145106]], 
+			labelPointer: [[1005966, 143902], [1032215, 138327]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'breezy point 219': {
-			lineString: [[1009254, 141718], [1007182, 140778], [1004474, 139375]], 
-			labelPointer: [[1006930, 140649], [1032215, 133394]],
-			alignPointer: 'left',
+			lineString: [[1009346, 141632], [1007274, 140693], [1004566, 139290]], 
+			labelPointer: [[1006930, 140649], [1032215, 133394]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'cedar grove': {
-			lineString: [[955848, 142084], [956081, 142696], [956316, 143001], [956558, 143050]], 
+			lineString: [[955699, 142003], [956081, 142696], [956316, 143001], [956814, 143156]], 
 			labelPointer: [[956203, 142567], [944521, 142567]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'orchard beach': {
-			lineString: [[1041773, 253306], [1041489, 253872], [1041645, 254958], [1042211, 255715], [1042878, 256033], [1043553, 256001]], 
-			labelPointer: [[1041892, 255289], [1053437, 255289]],
-			alignPointer: 'left',
+			lineString: [[1041808, 253255], [1041599, 253412], [1041408, 253664], [1041311, 254011], [1041304, 254528], [1041467, 255097], [1041699, 255496], [1042033, 255854], [1042559, 256164], [1042967, 256264], [1043314, 256251], [1043685, 256134]], 
+			labelPointer: [[1041892, 255289], [1053437, 255289]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'west fordham street association': {
-			lineString: [[1041954, 247676], [1041996, 247580]], 
+			lineString: [[1042046, 247776], [1042093, 247664]], 
 			labelPointer: [[1042038, 247708], [1053437, 248708]], 
-			alignPointer: 'left',
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'morris yacht and beach club': {
-			lineString: [[1043882, 244278], [1043726, 244482]], 
-			labelPointer: [[1043749, 244453], [1053437, 243453]],
-			alignPointer: 'left',
+			lineString: [[1044000, 244246], [1043954, 244359], [1043882, 244456], [1043751, 244504]], 
+			labelPointer: [[1043749, 244453], [1053437, 243453]], 
+			alignPointer: 'left', 
 			alignLine: 'left'
-		},
+		}, 
 		'douglaston manor association': {
-			lineString: [[1052137, 222607], [1052221, 222846]], 
-			labelPointer: [[1052076, 222669], [1040516, 222669]],
-			alignPointer: 'right',
+			lineString: [[1052167, 222478], [1052192, 222596], [1052234, 222735], [1052289, 222866]], 
+			labelPointer: [[1052076, 222669], [1040516, 222669]], 
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		
 		'trinity danish': {
-			lineString: [[1035369, 242532], [1035343, 242656]], 
+			lineString: [[1035234, 242490], [1035219, 242542], [1035219, 242582]], 
 			labelPointer: [[1035262, 242545], [1017736, 259100]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'right'
-		},
+		}, 
 		'white cross fishing club': {
-			lineString: [[1035447, 242239], [1035412, 242386]], 
+			lineString: [[1035359, 242167], [1035318, 242290]], 
 			labelPointer: [[1035363, 242249], [1017736, 253598]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'american turner': {
-			lineString: [[1035499, 242104], [1035438, 242228]], 
+			lineString: [[1035447, 242027], [1035379, 242147]], 
 			labelPointer: [[1035411, 242120], [1017736, 247596]], 
-			alignPointer: 'right',
+			alignPointer: 'right', 
 			alignLine: 'right'
-		},
+		}, 
 		'danish american beach club': {
-			lineString: [[1035765, 241734], [1035834, 241611]], 
-			labelPointer: [[1035716, 241574],[1017736, 241594]], 
-			alignPointer: 'right',
+			lineString: [[1035663, 241644], [1035732, 241520]], 
+			labelPointer: [[1035716, 241574], [1017736, 241594]], 
+			alignPointer: 'right', 
 			alignLine: 'left'
-		},
+		}, 
 		'manhem club': {
 			lineString: [[1035842, 241609], [1035877, 241530]], 
-			labelPointer: [[1035779, 241474], [1017736, 235592]],
-			alignPointer: 'right',
+			labelPointer: [[1035779, 241474], [1017736, 235592]], 
+			alignPointer: 'right', 
 			alignLine: 'right'
 		}
-	},
+	}, 
 	status: {
-		open: {msg: 'swimming and bathing permitted', css: 'open', rgb: [50, 255, 50]},
-		advisory: {msg: 'swimming and bathing not recommended', css: 'advisory', rgb: [255, 255, 50]},
-		'wet weather advisory': {msg: 'swimming and bathing not recommended', css: 'wet-advisory', rgb: [255, 255, 50]},
-		closed: {msg: 'swimming and bathing temporarily not permitted', css: 'closed', rgb: [255, 50, 50]},
+		open: {msg: 'swimming and bathing permitted', css: 'open', rgb: [50, 255, 50]}, 
+		advisory: {msg: 'swimming and bathing not recommended', css: 'advisory', rgb: [255, 255, 50]}, 
+		'wet weather advisory': {msg: 'swimming and bathing not recommended', css: 'wet-advisory', rgb: [255, 255, 50]}, 
+		closed: {msg: 'swimming and bathing temporarily not permitted', css: 'closed', rgb: [255, 50, 50]}, 
 		'closed for the season': {msg: '', css: 'off-season', rgb: [0, 0, 139]}
-	},
+	}, 
 	languages: {
-		en: {val: 'English', desc: 'English'},
-		ar: {val: 'Arabic', desc: '&#x627;&#x644;&#x639;&#x631;&#x628;&#x64A;&#x629;' /* Ã˜Â§Ã™â€žÃ˜Â¹Ã˜Â±Ã˜Â¨Ã™Å Ã˜Â© */},
-		bn: {val: 'Bengali', desc: '&#x9AC;&#x9BE;&#x999;&#x9BE;&#x9B2;&#x9BF;' /* Ã Â¦Â¬Ã Â¦Â¾Ã Â¦â„¢Ã Â¦Â¾Ã Â¦Â²Ã Â¦Â¿ */},
-		'zh-CN': {val: 'Chinese (Simplified)', desc: '&#x4E2D;&#x56FD;' /* Ã¤Â¸Â­Ã¥â€ºÂ½ */},
-		fr: {val: 'French', desc: 'Fran&#231;ais' /* FranÃƒÂ§ais */},
-		ht: {val: 'Haitian Creole', desc: 'Krey&#242;l Ayisyen' /* KreyÃƒÂ²l Ayisyen */},
-		ko: {val: 'Korean', desc: '&#xD55C;&#xAD6D;&#xC758;' /* Ã­â€¢Å“ÃªÂµÂ­Ã¬ï¿½Ëœ */},
-		ru: {val: 'Russian', desc: 'P&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;' /* PÃ‘Æ’Ã‘ï¿½Ã‘ï¿½Ã�ÂºÃ�Â¸Ã�Â¹ */},
-		es: {val: 'Spanish', desc: 'Espa&#241;ol' /* EspaÃƒÂ±ol */, hint: 'Traducir'},
+		en: {val: 'English', desc: 'English'}, 
+		ar: {val: 'Arabic', desc: '&#x627;&#x644;&#x639;&#x631;&#x628;&#x64A;&#x629;' /* Ã˜Â§Ã™â€žÃ˜Â¹Ã˜Â±Ã˜Â¨Ã™Å Ã˜Â© */}, 
+		bn: {val: 'Bengali', desc: '&#x9AC;&#x9BE;&#x999;&#x9BE;&#x9B2;&#x9BF;' /* Ã Â¦Â¬Ã Â¦Â¾Ã Â¦â„¢Ã Â¦Â¾Ã Â¦Â²Ã Â¦Â¿ */}, 
+		'zh-CN': {val: 'Chinese (Simplified)', desc: '&#x4E2D;&#x56FD;' /* Ã¤Â¸Â­Ã¥â€ºÂ½ */}, 
+		fr: {val: 'French', desc: 'Fran&#231;ais' /* FranÃƒÂ§ais */}, 
+		ht: {val: 'Haitian Creole', desc: 'Krey&#242;l Ayisyen' /* KreyÃƒÂ²l Ayisyen */}, 
+		ko: {val: 'Korean', desc: '&#xD55C;&#xAD6D;&#xC758;' /* Ã­â€¢Å“ÃªÂµÂ­Ã¬ï¿½Ëœ */}, 
+		ru: {val: 'Russian', desc: 'P&#x443;&#x441;&#x441;&#x43A;&#x438;&#x439;' /* PÃ‘Æ’Ã‘ï¿½Ã‘ï¿½Ã�ÂºÃ�Â¸Ã�Â¹ */}, 
+		es: {val: 'Spanish', desc: 'Espa&#241;ol' /* EspaÃƒÂ±ol */, hint: 'Traducir'}, 
 		ur: {val: 'Urdu', desc: '&#x627;&#x631;&#x62F;&#x648;' /* Ã˜Â§Ã˜Â±Ã˜Â¯Ã™Ë† */}
 	}
 };
